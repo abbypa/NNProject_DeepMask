3f6f0ce6e171a7fafe563629a26aa5d319777971
abbypa <abbypa@gmail.com>
2016-04-23 23:43:18 +0300

    path variables, train&test sets, prediction  not on every evaluation, prediction accuracy test, automatic expected score calculation (according to file prefix- pos/neg), refactoring

--- EndToEnd.py
+++ EndToEnd.py
@@ -1,4 +1,5 @@
 import datetime
+import glob
 import os
 from Constants import mask_threshold
 from FullNetGenerator import *
@@ -11,37 +12,75 @@ sgd_lr = 0.001
 sgd_decay = 0.00005
 sgd_momentum = 0.9
 
+epochs = 1
 batch_size = 32
-last_i = 3
-epochs = 50
-epochs_to_backup_weights = 5
+evaluation_batch_size = batch_size
+critical_loss = 500
 
+first_round = 1
+
+rounds = 10
+rounds_to_backup_weights = 5
+rounds_to_predict_results = 2
+
+
+# paths:
 graph_arch_path = 'Resources/graph_architecture_with_transfer.json'
 graph_weights_path = 'Resources/graph_weights_with_transfer.h5'
 original_net_weights_path = 'Resources/vgg16_graph_weights.h5'
-critical_loss = 500
+train_predictions_path = 'Predictions/train_predictions'
+test_predictions_path = 'Predictions/test_predictions'
+nets_dir_path = 'Predictions/nets'
+loss_file_path = 'Predictions/out-loss.csv'
+score_predictions_file_path = 'Predictions/score_predictions.csv'
+train_images_path = 'Predictions/train'
+test_images_path = 'Predictions/test'
 
 
 def print_debug(str_to_print):
     print '%s: %s' % (datetime.datetime.now(), str_to_print)
 
 
-def test_prediction(imgs, round_num, net, expected_result_arr, expected_masks, out):
-    predictions = net.predict({'input': imgs})
-    score_predictions = predictions['score_output'].flatten()
-    print_debug('prediction %s' % score_predictions)
-    evaluation = net.evaluate({'input': imgs, 'score_output': expected_result_arr, 'seg_output': expected_masks},
-                              batch_size=1)
-    print_debug('evaluation loss %s' % evaluation)
-    out.write('%s,%s,%s\n' % (datetime.datetime.now(), evaluation, score_predictions))
-    out.flush()
+def evaluate_net_loss(net, train_images, train_expected_scores, train_expected_masks,
+                      test_images, test_expected_scores, test_expected_masks, loss_file):
+    train_loss = net.evaluate({'input': train_images, 'score_output': train_expected_scores,
+                               'seg_output': train_expected_masks}, batch_size=evaluation_batch_size, verbose=0)
+    test_loss = net.evaluate({'input': test_images, 'score_output': test_expected_scores,
+                              'seg_output': test_expected_masks}, batch_size=evaluation_batch_size, verbose=0)
+    print_debug('evaluation- train loss %s test loss %s' % (train_loss, test_loss))
+    loss_file.write('%s,%s,%s\n' % (datetime.datetime.now(), train_loss, test_loss))
+    loss_file.flush()
+    return train_loss, test_loss
+
+
+def evaluate_net_predictions_if_needed(net, round_num, train_images, test_images,
+                                       train_expected_scores, test_expected_scores, score_predictions_file):
+    if round_num % rounds_to_predict_results == 0:
+        print_debug('evaluating train predictions:')
+        correct_scores_train = evaluate_net_predictions(net, round_num, train_images, train_predictions_path,
+                                                        train_expected_scores)
+        print_debug('evaluating test predictions:')
+        correct_scores_test = evaluate_net_predictions(net, round_num, test_images, test_predictions_path,
+                                                       test_expected_scores)
+        score_predictions_file.write('%d,%d,%d,%d\n' % (correct_scores_train, len(train_expected_scores),
+                                                        correct_scores_test, len(test_expected_scores)))
+        score_predictions_file.flush()
+
+
+def evaluate_net_predictions(net, round_num, images, predictions_path, expected_scores):
+    predictions = net.predict({'input': images})
+    score_predictions = predictions['score_output']
+    # correct predictions have the same sign
+    correct_predictions = sum(map(lambda net_score, expected_score: np.sign(net_score) == np.sign(expected_score),
+                                  score_predictions, expected_scores))
+    print_debug('%d/%d correct prediction' % (correct_predictions, len(score_predictions)))
 
     for i in range(len(predictions['seg_output'])):
         mask = predictions['seg_output'][i]
-        prediction_path = 'Predictions/round%d-pic%d.png' % (round_num, i)
+        prediction_path = '%s/round%d-pic%d.png' % (predictions_path, round_num, i)
         binarize_and_save_mask(mask, mask_threshold, prediction_path)
 
-    return evaluation
+    return correct_predictions
 
 
 def saved_net_exists():
@@ -79,29 +118,51 @@ def save_net(net):
     net.save_weights(graph_weights_path)
 
 
-def prepare_data():
-    print_debug('preparing data...')
-    img_paths = [
-        'Predictions/49275-427041-0-0-1-1-im.png',
-        'Predictions/131909-1304103-0-0-1-1-mir-im.png',
-        'Predictions/155749-1441236-0-0-1-1-im.png',
-        ]
-    images = prepare_local_images(img_paths)
+def example_name_to_result(ex_name):
+    if ex_name.startswith('pos'):
+        return 1
+    elif ex_name.startswith('neg'):
+        return -1
+    else:
+        print 'illegal example: %s' % ex_name
+        raise Exception
 
-    expected_mask_paths = [str.replace(img_path, 'im', 'mask') for img_path in img_paths]
+
+def prepare_data(examples_path):
+    ex_paths = glob.glob('%s/*-im.png' % examples_path)
+    np.random.shuffle(ex_paths)
+    images = prepare_local_images(ex_paths)
+
+    expected_mask_paths = [str.replace(img_path, 'im', 'mask') for img_path in ex_paths]
     expected_masks = prepare_expected_masks(expected_mask_paths)
 
-    expected_results = [1, 1, 1]
-
+    expected_results = [example_name_to_result(os.path.basename(ex_path)) for ex_path in ex_paths]
     expected_result_arr = np.array([[res] for res in expected_results])
 
     return [images, expected_result_arr, expected_masks]
 
 
+def backup_net(graph, round_num):
+    print_debug("Saving latest net weights")
+    graph.save_weights('%s/latest_net' % nets_dir_path, overwrite=True)
+
+    print_debug("Saving net weights for round %d" % round_num)
+    if round_num % rounds_to_backup_weights == 0:
+        graph.save_weights('%s/net-round%d' % (nets_dir_path, round_num))
+
+
+def is_exploding_loss(loss):
+    math.isnan(loss) or math.isinf(loss) or loss >= critical_loss
+
+
 def main():
-    losses = []
-    out_path = 'Predictions/out-loss.csv'
-    out = open(out_path, 'a')
+    train_losses = []
+    test_losses = []
+
+    loss_file = open(loss_file_path, 'a')
+    loss_file.write('time,train loss,test loss\n')
+    score_predictions_file = open(score_predictions_file_path, 'a')
+    score_predictions_file.write('train success, train total, test success, test total\n')
 
     if saved_net_exists():
         graph = load_saved_net()
@@ -110,29 +171,31 @@ def main():
         save_net(graph)
 
     compile_net(graph)  # current keras version cannot load compiled net with custom loss function
-    [images, expected_result_arr, expected_masks] = prepare_data()
+    print_debug('preparing data...')
+    [train_images, train_expected_scores, train_expected_masks] = prepare_data(train_images_path)
+    [test_images, test_expected_scores, test_expected_masks] = prepare_data(test_images_path)
 
-    print_debug('running net...')
-    losses.append(test_prediction(images, last_i, graph, expected_result_arr, expected_masks, out))
+    # uncomment to restore last run results / initial net result
+    # print_debug('running net...')
+    # losses.append(test_prediction(train_images, last_i, graph, train_expected_scores, train_expected_masks, out))
 
-    for i in range(last_i, epochs):
-        print_debug('starting round %d:' % (i+1))
-        graph.fit({'input': images, 'seg_output': expected_masks, 'score_output': expected_result_arr}, nb_epoch=1,
-                  verbose=0)
-        last_loss = test_prediction(images, i+1, graph, expected_result_arr, expected_masks, out)
-
-        if math.isnan(last_loss) or math.isinf(last_loss) or last_loss >= critical_loss:
-            print_debug("Loss %s too big- stopping" % losses[-1])
+    for round_number in range(first_round, rounds + 1):
+        print_debug('starting round %d:' % round_number)
+        graph.fit({'input': train_images, 'seg_output': train_expected_masks, 'score_output': train_expected_scores},
+                  nb_epoch=epochs, batch_size=batch_size, verbose=0, shuffle=True)
+        train_loss, test_loss = evaluate_net_loss(graph, train_images, train_expected_scores, train_expected_masks,
+                                                  test_images, test_expected_scores, test_expected_masks, loss_file)
+        if is_exploding_loss(train_loss):
+            print_debug("Loss %s too big- stopping" % train_loss)
             break
-        else:
-            losses.append(last_loss)
-            print_debug("Saving net weights for round %d" % (i+1))
-            graph.save_weights('Predictions/net', overwrite=True)
 
-        if i % epochs_to_backup_weights == 0:
-            graph.save_weights('Predictions/net%d' % i)
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
+        evaluate_net_predictions_if_needed(graph, round_number, train_images, test_images,
+                                           train_expected_scores, test_expected_scores, score_predictions_file)
+        backup_net(graph, round_number)
 
-    out.close()
+    loss_file.close()
 
 
 if __name__ == "__main__":
